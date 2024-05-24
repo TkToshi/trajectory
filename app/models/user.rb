4c893418c918830b4a58ba07d06056de9dc2b4df
TkToshi <k_y_t_active@yahoo.co.jp>
2024-05-24 12:21:25 +0900

    アカウント削除機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :profile
-  has_many :posts
-  has_many :likes
+  has_one :profile, dependent: :destroy # 他の関連付けやバリデーションなど
+  has_many :posts, dependent: :destroy # 他の関連付けやバリデーションなど
+  has_many :likes, dependent: :destroy # 他の関連付けやバリデーションなど
 
   has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id
   has_many :followings, through: :active_relationships, source: :follower
@@ -13,8 +13,17 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
 
+  before_destroy :delete_associated_posts # ユーザーが削除されるときに、関連する投稿も削除する
+
   def followed_by?(user)
     follower = passive_relationships.find_by(following_id: user.id)
     follower.present?
   end
+
+  private
+
+  def delete_associated_posts
+    self.posts.destroy_all
+  end
+
 end
